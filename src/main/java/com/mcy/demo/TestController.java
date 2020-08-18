bc379e6c7878e8afabef134600be46fd4ad9b968
‘ <king_mcy@163.com>
2020-08-18 22:32:38 +0800

    添加测试controller

--- src/main/java/com/mcy/demo/TestController.java
+++ src/main/java/com/mcy/demo/TestController.java
@@ -3,8 +3,6 @@ package com.mcy.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class TestController {
